1e3463dfb8bc4fa47a7f3656ec180a94d14291b2
Dan Marchese <marchese.daniel@gmail.com>
2015-10-28 00:07:13 -0400

    Initial commit with entropy implemented.

---
+++ kmeansdriver.py
@@ -0,0 +1,16 @@
+'''This is where the logic for driving the k-means algorithm sits.
+
+Author: Dan Marchese
+'''
+import os
+import sys
+
+def main():
+    '''The main entry point for the program.'''
+    pass
+
+if __name__ == '__main__':
+    try:
+        sys.exit(main())
+    except Exception as e:
+        sys.exit(str(e))
---
+++ util/quality.py
@@ -0,0 +1,31 @@
+'''Various measures of clustering quality.
+
+Author: Dan Marchese
+'''
+import math
+
+def entropy(clustering):
+    '''Calculates the entropy of a given clustering.'''
+
+    # Calculate the total number of points in the clustering.
+    total_points = float(reduce(lambda x, y: x + sum(y.values()), clustering, initializer=0))
+
+    # Used to accumulate the entropy values for each cluster.
+    ent = 0.0
+
+    # Calculate the entropy of each cluster.
+    for cluster in clustering:
+        sub_ent = 0.0
+
+        # Find the number of points in this cluster.
+        num_points = float(sum(cluster.values()))
+
+        # Calculate the entropy for each label.
+        for label in cluster.keys():
+            p = cluster[label] / num_points
+            sub_ent += p * math.log(1.0 / p, 2)
+
+        # Put this cluster into the total entropy accumulator
+        ent += (num_points / total_points) * sub_ent
+
+    return ent
